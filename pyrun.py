1c98d661bda180b123951a70ac5539ffa389ea25
chausies <chausies@berkeley.edu>
2015-08-21 23:02:20 +1000

    Did nothing important

--- pyrun.py
+++ pyrun.py
@@ -2,9 +2,9 @@ from sys import argv
 for f in argv[1:]:
   execfile(f)
 import readline
-import code
 import rlcompleter
 readline.parse_and_bind("tab: complete")
+import code
 vars = globals().copy()
 vars.update(locals())
 shell = code.InteractiveConsole(vars)
