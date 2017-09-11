f2b2772ca5d7e5d37f0957b59eb61a9286d067b5
Ajay Tripathi <chausies7@gmail.com>
2017-09-10 21:25:55 -0700

    Updated RUN_THIS.sh to make .bashrc source new custom .mybashrc

--- RUN_THIS.sh
+++ RUN_THIS.sh
@@ -1,4 +1,16 @@
-ln -s ~/configs/.bashrc  ~/
+# Get .bashrc to source .mybashrc if it isn't already
+if -Fxq "source ~/.mybashrc" ~/.bashrc
+then
+echo ".bashrc already sourcing .mybashrc"
+else
+cat <<EOT >> ~/.bashrc
+
+# Run all the custom configs in .mybashrc
+source ~/.mybashrc
+
+EOT
+fi
+
 ln -s ~/configs/.inputrc ~/
 ln -s ~/configs/.pythonrc ~/
 ln -s ~/configs/.gitconfig ~/
