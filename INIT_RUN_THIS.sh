38a2456636db7b43448528e34c8c60340fcab520
chausies <chausies@berkeley.edu>
2017-10-31 05:28:19 -0700

    Added install of texlive-fonts-extra

--- INIT_RUN_THIS.sh
+++ INIT_RUN_THIS.sh
@@ -33,6 +33,7 @@ sudo -H pip install cvxpy progressbar2 youtube-dl
 # Install LaTeX stuff (like pdflatex command)
 sudo apt-get install texlive
 sudo apt-get install texlive-latex-extra --no-install-recommends
+sudo apt-get install texlive-fonts-extra # Gives fonts like bbm for \mathbbm{1}
 sudo apt-get install dvipng
 sudo apt-get install python-pygments
 
